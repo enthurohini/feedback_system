2b829051095b97b182aa069b4c0746a9b9344512
rohinichoudhary <chaudhary.rohini241@gmail.com>
2015-01-12 13:45:15 +0530

    providing facility to maintain database by admin.

--- feedback_form/admin.py
+++ feedback_form/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from feedback_form.models import feedback_student_info, infrastructure_support_info
+from feedback_form.models import feedback_student_info, infrastructure_support_info, academic_assessment_info, course, batch, section_info, faculty_table, subject, time_table
 
 # Register your models here.
 
 admin.site.register(feedback_student_info)
 admin.site.register(infrastructure_support_info)
+admin.site.register(academic_assessment_info)
+admin.site.register(course)
+admin.site.register(batch)
+admin.site.register(section_info)
+admin.site.register(faculty_table)
+admin.site.register(subject)
+admin.site.register(time_table)
+
