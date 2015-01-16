b2dbb38d107657383753d32ab71d461f5f8d83e5
rohinichoudhary <chaudhary.rohini241@gmail.com>
2015-01-16 11:04:09 +0530

    adding url for feedback login page

--- feedback_system/urls.py
+++ feedback_system/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     #url(r'^$', 'feedback_system.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^feedback_system/', include('feedback_form.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
