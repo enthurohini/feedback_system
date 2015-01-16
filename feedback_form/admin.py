33caa490803d3498621d5af886924ce117b3103d
rohinichoudhary <chaudhary.rohini241@gmail.com>
2015-01-16 10:58:46 +0530

    Adding additional functionality to admin module

--- feedback_form/admin.py
+++ feedback_form/admin.py
@@ -2,11 +2,29 @@ from django.contrib import admin
 from feedback_form.models import feedback_student_info, infrastructure_support_info, academic_assessment_info, course, batch, section_info, faculty_table, subject, time_table
 
 # Register your models here.
-class infrastructureInline(admin.StackedInline):
+class batchAdmine(admin.ModelAdmin):
+	list_display = ('batch_id', 'course_id', 'session')
+
+class sectionAdmin(admin.ModelAdmin):
+	list_display = ('id','batch_id', 'section')	
+
+class courseAdmin(admin.ModelAdmin):
+	list_display = ('course_id', 'course_name', 'stream', 'number_of_sem')	
+
+class facultyAdmin(admin.ModelAdmin):
+	list_display = ('user_id', 'name')
+
+class subjectAdmin(admin.ModelAdmin):
+	list_display = ('subject_id', 'name_of_subject', 'semester')
+
+class time_tableAdmin(admin.ModelAdmin):
+	list_display = ('s_no', 'subject_id', 'faculty_id', 'batch_id', 'room_number')
+
+class infrastructureInline(admin.TabularInline):
 	model = infrastructure_support_info
 	extra = 1
 
-class academicInline(admin.StackedInline):
+class academicInline(admin.TabularInline):
 	model = academic_assessment_info
 	extra = 1
 
@@ -16,14 +34,15 @@ class studentAdmin(admin.ModelAdmin):
 		#(None, {'fields': ['books_availability']})
 	]
 	inlines = [infrastructureInline, academicInline]
+	list_display = ('fs_id', 'batch_id', 'course', 'semester', 'section', 'feedback_session')
 
 admin.site.register(feedback_student_info, studentAdmin)
 #admin.site.register(infrastructure_support_info)
 #admin.site.register(academic_assessment_info)
-admin.site.register(course)
-admin.site.register(batch)
-admin.site.register(section_info)
-admin.site.register(faculty_table)
-admin.site.register(subject)
-admin.site.register(time_table)
+admin.site.register(course, courseAdmin)
+admin.site.register(batch, batchAdmine)
+admin.site.register(section_info, sectionAdmin)
+admin.site.register(faculty_table, facultyAdmin)
+admin.site.register(subject, subjectAdmin)
+admin.site.register(time_table, time_tableAdmin)
 
