94bfee1627a348ab137ae8c7f561b264f26ab55c
rohinichoudhary <chaudhary.rohini241@gmail.com>
2015-01-20 09:52:10 +0530

    new url is created for login form

--- feedback_form/urls.py
+++ feedback_form/urls.py
@@ -5,6 +5,8 @@ from feedback_form import views
 urlpatterns = patterns('', 
 		url(r'^$', views.index, name = 'index'),
 		url(r'^action/$', views.action, name = 'action'),
+		url(r'^login/$', views.login, name = 'login'),
+		url(r'^thanks/$', views.thanks, name = 'thanks'),
 	)
 
 
