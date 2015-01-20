d320a6922499f6ad533b6308c0be20df358f1583
rohinichoudhary <chaudhary.rohini241@gmail.com>
2015-01-20 17:20:59 +0530

    solved the error of action field in form

--- feedback_form/forms.py
+++ feedback_form/forms.py
@@ -1,7 +1,8 @@
 from django import forms
+from feedback_form.models import course
 
 class loginForm(forms.Form):
-	course = forms.CharField(max_length=100)
+	course = forms.ModelChoiceField(queryset=course.objects.all())
 	semester = forms.IntegerField()
 	section = forms.CharField(max_length=100)
 	batch_id = forms.CharField(max_length=100)
