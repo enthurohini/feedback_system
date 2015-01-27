91cbdd6896fc487361d35fb6fb0284ace9f9d918
rohinichoudhary <chaudhary.rohini241@gmail.com>
2015-01-27 14:23:49 +0530

    for applying jquery but not working.

--- feedback_form/urls.py
+++ feedback_form/urls.py
@@ -2,11 +2,13 @@ from django.conf.urls import patterns, url
 
 from feedback_form import views
 
-urlpatterns = patterns('', 
+urlpatterns = patterns('',
 		url(r'^$', views.index, name = 'index'),
 		url(r'^action/$', views.action, name = 'action'),
 		url(r'^login/$', views.login, name = 'login'),
 		url(r'^thankyou/$', views.thanks, name = 'thanks'),
+		url(r'^ajax_color_request/$', views.ajax_color_request),
+		url(r'^get_batch/(?P<c_id>\w+)/$', views.get_batch, name = 'get_batch'),
 	)
 
 
--- feedback_form/views.py
+++ feedback_form/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, render_to_response
 from feedback_form.models import feedback_student_info
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
+
+#import json as simplejson
+import json
+import feedback_form.models
 #from django.template import RequestContext, loader
 
 from feedback_form.forms import loginForm
@@ -24,7 +28,7 @@ def action(request):
 	return HttpResponse(message)
 
 def login(request):
-	if request.method == 'POST':
+	if request.method == 'POST' and request.is_ajax:
 		form = loginForm(request.POST)
 		if form.is_valid():
 			return HttpResponseRedirect('/feedback_system/thankyou/')
@@ -36,3 +40,20 @@ def login(request):
 def thanks(request):
 	return render(request, 'feedback_form/thankyou.html')
 
+def ajax_color_request(request):
+    # Expect an auto 'type' to be passed in via Ajax and POST
+    if request.is_ajax() and request.method == 'POST':
+        course_course_name = course.objects.filter(name=request.POST.get('course_name', ''))
+        c_id = course.objects.raw('SELECT course_id FROM course WHERE course_name = %s', [course_course_name])
+        batch_id = batch.objects.raw('SELECT batch_id FROM batch WHERE course_id = %s', [c_id]) # get all the colors for this type of auto.
+    return render_to_response('feedback_form/loginForm.html', locals())
+
+def get_batch(request, c_id):
+    current_course = feedback_form.models.course.objects.get(course_name=c_id)
+    batches = feedback_form.models.batch.objects.all().filter(course_id=current_course)
+    batch_dict = {}
+    for batch in batches:
+        batch_dict[batch.batch_id] = batch.batch_id
+    return HttpResponse(json.dumps(batch_dict), mimetype="application/json")
+
+    #, mimetype="application/json"
