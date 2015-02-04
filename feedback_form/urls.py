cd53872085726e24702ffc34052791a190d050eb
rohinichoudhary <chaudhary.rohini241@gmail.com>
2015-02-04 09:58:14 +0530

    url for updating the sem is created.

--- feedback_form/urls.py
+++ feedback_form/urls.py
@@ -9,6 +9,7 @@ urlpatterns = patterns('',
 		url(r'^thankyou/$', views.thanks, name = 'thanks'),
 		url(r'^ajax_color_request/$', views.ajax_color_request),
 		url(r'^login/login/get_batch/(?P<c_id>\w+)/$', views.get_batch, name = 'get_batch'),
+		url(r'^login/login/get_no_of_sem/(?P<c_id>\w)/$', views.get_no_of_semester, name = 'get_no_of_sem'),
 	)
 
 
