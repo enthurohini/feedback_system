510c6551a823b72a63c89944b97b154ac8bcf38a
rohinichoudhary <chaudhary.rohini241@gmail.com>
2015-02-04 09:55:42 +0530

    model form for infrastructure was created but removed.

--- feedback_form/forms.py
+++ feedback_form/forms.py
@@ -3,12 +3,13 @@ from feedback_form.models import course, batch, section_info
 
 class loginForm(forms.Form):
 	course_name = forms.ModelChoiceField(queryset=course.objects.values_list('course_name', flat = True))
-	semester = forms.ModelChoiceField(queryset=course.objects.values_list('number_of_sem', flat = True))
+	#semester = forms.ModelChoiceField(queryset=course.objects.values_list('number_of_sem', flat = True))
+	semester = forms.ModelChoiceField(queryset=course.objects.none())
 	section = forms.ModelChoiceField(queryset=section_info.objects.values_list('section', flat = True).distinct())
 	batch = forms.ModelChoiceField(queryset=batch.objects.none())
 	feedback_session = forms.IntegerField()
 
 class Meta:
-        model = batch
-        fields = ('course_name', 'batch')
+        model = batch, course
+        fields = ('course_name', 'batch', 'semester')
 
