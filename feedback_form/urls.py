7e71af5d50ff5018cc6f674c55053937bb7ac11e
rohinichoudhary <chaudhary.rohini241@gmail.com>
2015-02-09 09:48:49 +0530

    url for infrastructure page is created.

--- feedback_form/urls.py
+++ feedback_form/urls.py
@@ -6,10 +6,10 @@ urlpatterns = patterns('',
 		url(r'^$', views.index, name = 'index'),
 		url(r'^action/$', views.action, name = 'action'),
 		url(r'^login/$', views.login, name = 'login'),
+		url(r'^infrastructure_support/$', views.infrastructure_support, name = 'infrastructure_support'),
 		url(r'^thankyou/$', views.thanks, name = 'thanks'),
 		url(r'^ajax_color_request/$', views.ajax_color_request),
 		url(r'^login/login/get_batch/(?P<c_id>\w+)/$', views.get_batch, name = 'get_batch'),
-		url(r'^login/login/get_no_of_sem/(?P<c_id>\w)/$', views.get_no_of_semester, name = 'get_no_of_sem'),
 	)
 
 
