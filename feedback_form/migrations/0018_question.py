f52e9053c61d2dc4cd8ec158f2fd0a2376a6e208
rohinichoudhary <chaudhary.rohini241@gmail.com>
2015-02-11 10:25:42 +0530

    migrations are applied for question model

---
+++ feedback_form/migrations/0018_question.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('feedback_form', '0017_auto_20150114_1514'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Question',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('question_text', models.TextField()),
+                ('type', models.CharField(max_length=50)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
