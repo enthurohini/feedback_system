96c6542fe910d52d65a5bcacdb55175e81e5d745
swatimahajan <mahajanswati108@gmail.com>
2015-04-13 08:10:05 +0530

    added form heading in student personal registration form

--- student_registration/forms.py
+++ student_registration/forms.py
@@ -1,8 +1,10 @@
 ''' ~~~ Import Statements ~~~ '''
 from django import forms 
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit, Layout, Field ,Fieldset
-from crispy_forms.bootstrap import (PrependedText, PrependedAppendedText, FormActions)
+from crispy_forms.layout import Submit, Layout, Field ,Fieldset,Div,Row
+from crispy_forms.bootstrap import (PrependedText, PrependedAppendedText,InlineField, FormActions)
+
+
 
 class student_personal_infoForm(forms.Form):
 
@@ -10,8 +12,9 @@ class student_personal_infoForm(forms.Form):
 	CATEGORY = ('GENERAL','OBC','ST','SC')
 
 	first_name = forms.CharField(label="First Name",required=True,max_length=20, widget = forms.TextInput(attrs={'placeholder':'First Name'}))
-	middle_name = forms.CharField(label="Middle Name", required=False,max_length=20, widget = forms.TextInput(attrs={'placeholder':'Middle Name'}))
-	last_name= forms.CharField(label="Last Name", required=True,max_length=20,widget = forms.TextInput(attrs={'placeholder':'Last Name'}))
+	middle_name = forms.CharField(label="",required=False,max_length=20, widget = forms.TextInput(attrs={'placeholder':'Middle Name'}))
+	last_name= forms.CharField(label="", required=True,max_length=20,widget = forms.TextInput(attrs={'placeholder':'Last Name'}))
+	
 	father_name = forms.CharField(label="Father's Name", required=True,max_length=20,widget = forms.TextInput(attrs={'placeholder':'Father Name'}))
 	mother_name = forms.CharField(label="Mother's Name", required=True,max_length=20,widget = forms.TextInput(attrs={'placeholder':'Mother Name'}))
 	birth_date = forms.CharField(label="Birth date", required=True,widget = forms.TextInput(attrs={'placeholder':'Birth Date'}))
@@ -26,26 +29,38 @@ class student_personal_infoForm(forms.Form):
 	helper = FormHelper()
 	helper.form_method = 'POST'
 	helper.label_class = 'col-md-4'
-	helper.form_class = 'form_horizontal form-inline'
+	helper.form_class = 'form-inline'
 	helper.field_class = 'col-md-4 control-label'
 	helper.layout = Layout(
-	    
-    Field('first_name', css_class='control-label'),
-    Field('middle_name', css_class='control-label'),
-    Field('last_name', css_class='control-label'),
-    Field('father_name', css_class='control-label'),
-    Field('mother_name', css_class='control-label'),
+	    Row(  
+            Field('first_name',css_class='control-label'),
+            Field('middle_name',css_class='control-label'), 
+            Field('last_name',css_class='control-label'),
+        css_class='row-fluid'),
+        Row(
+            Field('father_name', css_class='control-label'),
+            Field('mother_name', css_class='control-label'),
+        css_class='row-fluid'),    
     Field('birth_date', css_class='control-label'),
-    Field('gender', css_class='control-label'),
-    Field('category', css_class='input-md'),
-    PrependedAppendedText('mobile_number', '+91',),        
-    Field('telephone_number', css_class='input-md'),
-    Field('email', css_class='input-md'),
-    Field('current_address', css_class='input-lg'),
-    Field('permanent_address', css_class='input-lg'),
-    
+       Row(
+           Field('gender', css_class='control-label'),
+           Field('category', css_class='input-md'),
+        css_class='row-fluid'),
+       Row(
+          PrependedAppendedText('mobile_number', '+91',),        
+          Field('telephone_number', css_class='input-md'),
+        css_class='row-fluid'),
+
+    InlineField(
+    	Field('email', css_class='input-md'),	
+       ),
+    InlineField(   	
+        Field('current_address', css_class='input-lg'),
+       ),
+        Field('permanent_address', css_class='input-lg'),
+   
     FormActions(Submit('Submit', 'Submit', css_class='btn-primary'))
     )
-
+ 
 	
     
